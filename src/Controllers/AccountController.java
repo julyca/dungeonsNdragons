439cf13e1de70c71e1d86ae856d6b1f064a397b3
julyca <jufsantana1@gmail.com>
2021-03-05 10:13:41 -0300

    verify password (md5 hash)

--- src/Controllers/AccountController.java
+++ src/Controllers/AccountController.java
@@ -1,5 +1,27 @@
 package Controllers;
 
+import java.security.*;
+import java.math.*;
+
 public class AccountController {
-    
+
+    public String TextToMD5(String s) throws NoSuchAlgorithmException {
+        MessageDigest m = MessageDigest.getInstance("MD5");
+        m.update(s.getBytes(),0,s.length());
+        return new BigInteger(1,m.digest()).toString(16);
+    }
+
+    public Boolean CheckPassword(String pw, String UserNickname){
+        Boolean r = false;
+
+        try {
+            // TODO: Get user password from file/db
+            if("94f9eb627104fc845739ee95880053c6".equals(TextToMD5(pw))){
+                r = true;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+        return r;
+    }
 }
